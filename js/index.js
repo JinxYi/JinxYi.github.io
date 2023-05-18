cc26292042ba7e314fe0798359e8cc9fa52bacec
Jing Yi <jingyi0939@gmail.com>
2023-05-18 17:28:32 +0800

    fixed responsive nav

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 
+const menu_icon = document.querySelector("#menu-bar-icon");
+const nav_list = document.querySelector(".nav-list");
 
 // responsive nav
 document.onload = () => {
     
 }
 
+menu_icon.addEventListener("click", () => {
+    nav_list.classList.toggle("show");
+}); 
